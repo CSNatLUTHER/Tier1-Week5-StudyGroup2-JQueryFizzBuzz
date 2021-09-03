9a43a5199690a908cb817b551bd5db7674f4d9d5
Miriam McNamara <miriammcnamara@miriamscomputer.lan>
2021-09-03 12:30:49 -0500

    button and box in function

--- scripts/client.js
+++ scripts/client.js
@@ -1,10 +1,11 @@
 $( document ).ready( onReady );
 
-function onReady(){
-    console.log( 'JQ');
+function onReady(){;
+    $( `#newNumberButton` ).on( 'click', fizzBuzz);
 }
 
 function fizzBuzz( newNumber ){
+    newNumber=$( '#newNumberBox' ).val();
     let answer='';
     if (newNumber%3===0){
        answer+='Fizz';
@@ -15,10 +16,21 @@ function fizzBuzz( newNumber ){
     else if ((newNumber%3!=0) && (newNumber%5!=0)){
         answer+=newNumber;
     }
+    console.log(answer);
     return answer;
 }
 
-console.log(fizzBuzz( 10 ));
-console.log(fizzBuzz ( 9 ));
-console.log(fizzBuzz ( 15 ));
-console.log(fizzBuzz (23));
+// console.log(fizzBuzz( 10 ));
+// console.log(fizzBuzz ( 9 ));
+// console.log(fizzBuzz ( 15 ));
+// console.log(fizzBuzz (23));
+
+//place for someone to enter the number: text field
+//button to submit
+//place to display the answer: p tag
+
+//create a function that will take the number input from the new number box and use it in the function
+
+
+//when the button is clicked, run the function
+//create a p tag to display the results
