c97deb1041e7f0d2fa56044b4736bf5666638ba7
Miriam McNamara <miriammcnamara@miriamscomputer.lan>
2021-09-03 12:59:52 -0500

    fully up and running

--- scripts/client.js
+++ scripts/client.js
@@ -1,36 +1,40 @@
 $( document ).ready( onReady );
+//hey JQ, when html doc is on the DOM, run the function onReady;
 
 function onReady(){;
     $( `#newNumberButton` ).on( 'click', fizzBuzz);
+    //hey JQ, when the newNumberButton gets clicked on, run the function fizzBuzz;
 }
+
 
 function fizzBuzz( newNumber ){
     newNumber=$( '#newNumberBox' ).val();
+    //the input into this function should be the input in the newNumberBox, JQ do that;
     let answer='';
+    //new variable that is an empty string;
     if (newNumber%3===0){
        answer+='Fizz';
+       //if the number is divisible by 3, 'fizz' is added to the variable string;
     }
     if (newNumber%5===0){
         answer+="Buzz";
+        //if the number is divisible by 5, 'buzz' is added to the variable string;
     }
     else if ((newNumber%3!=0) && (newNumber%5!=0)){
         answer+=newNumber;
+        //else if the number is divisible by 3 and divisible by 5, 'fizzbuzz' is added to the variable string;
     }
-    console.log(answer);
+    //once all that is figured out,
+    let el= $( `#mainDiv` );
+    //use JQ to assign the mainDiv to a variable;
+    el.append( '<p id="answer"></p>' );
+    //append a p tag to the mainDiv;
+    $( `#answer` ).empty();
+    //empty out the p tag (only needed if the function runs more than once)
+    $( `#answer` ).append( answer );
+    //append the answer to the p tag
     return answer;
 }
 
-// console.log(fizzBuzz( 10 ));
-// console.log(fizzBuzz ( 9 ));
-// console.log(fizzBuzz ( 15 ));
-// console.log(fizzBuzz (23));
 
-//place for someone to enter the number: text field
-//button to submit
-//place to display the answer: p tag
-
-//create a function that will take the number input from the new number box and use it in the function
-
-
-//when the button is clicked, run the function
-//create a p tag to display the results
+ 
