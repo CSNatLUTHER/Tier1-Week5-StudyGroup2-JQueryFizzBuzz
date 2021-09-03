245c25a2d8183d46c994d8ab495af45a0551cbe2
Miriam McNamara <miriammcnamara@miriamscomputer.lan>
2021-09-03 12:06:19 -0500

    fizzbuzz function

--- scripts/client.js
+++ scripts/client.js
@@ -3,3 +3,22 @@ $( document ).ready( onReady );
 function onReady(){
     console.log( 'JQ');
 }
+
+function fizzBuzz( newNumber ){
+    let answer='';
+    if (newNumber%3===0){
+       answer+='Fizz';
+    }
+    if (newNumber%5===0){
+        answer+="Buzz";
+    }
+    else if ((newNumber%3!=0) && (newNumber%5!=0)){
+        answer+=newNumber;
+    }
+    return answer;
+}
+
+console.log(fizzBuzz( 10 ));
+console.log(fizzBuzz ( 9 ));
+console.log(fizzBuzz ( 15 ));
+console.log(fizzBuzz (23));
